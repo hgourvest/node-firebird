426553740d6696f97046ae401e4478cf5f9f4bd4
Ben Hanna <benpaulhanna@gmail.com>
2018-06-01 17:09:27 +0000

    Converted test file back to ES5 for compatibility.
    Clean up and reverted the change to email notifications.

--- test/index.js
+++ test/index.js
@@ -1,73 +1,81 @@
-const Firebird = require('../lib');
+var Firebird = require('../lib');
 
-const assert = require('assert');
-const fs = require('fs');
-const path = require('path');
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
 
-const currentDate = new Date();
-const testDir = path.resolve(__dirname);
+var currentDate = new Date();
+var testDir = path.resolve(__dirname);
 
-const config = {
-    database: path.join(process.env.FIREBIRD_DATA || testDir, `test-${currentDate.getTime()}.fdb`),
+var config = {
+    database: path.join(process.env.FIREBIRD_DATA || testDir, 'test-' + currentDate.getTime() + '.fdb'),
     host: '127.0.0.1',
     port: 3050,
     user: 'sysdba',
     password: 'masterkey',
+    role: null,
+    pageSize: 4096,
+    timeout: 3000,
     lowercase_keys: true
 };
 
-describe('Connection', () => {
+describe('Connection', function () {
 
-    it('should attach or create database', done => {
-        Firebird.attachOrCreate(config, (err, db) => {
+    it('should attach or create database', function (done) {
+        Firebird.attachOrCreate(config, function (err, db) {
             assert.ok(!err, err);
+            
             db.detach();
             done();
         });
     });
 
-    it('should reconnect when socket is closed', done => {
-        Firebird.attach(config, (err, db) => {
+    it('should reconnect when socket is closed', function (done) {
+        Firebird.attach(config, function (err, db) {
             assert.ok(!err, err);
-            db.on('reconnect', () => {
+            
+            db.connection._socket.end();
+            db.on('reconnect', function () {
                 db.detach();
                 done();
             });
-            db.connection._socket.end();
         });
     });
 });
 
-describe('Pooling', () => {
+describe('Pooling', function () {
         
-    const poolSize = 2;
+    var poolSize = 2;
+    var pool;
     
-    let pool;
-    
-    before(() => {
+    before(function () {
         pool = Firebird.pool(poolSize, config);
     });
     
-    after(() => {
+    after(function () {
         pool.destroy();
     });
     
-    it('should wait when all connections are in use', done => {
-        for (let i = 0; i < poolSize; i++) {
-            pool.get((err, db) => {
+    it('should wait when all connections are in use', function (done) {
+        for (var i = 0; i < poolSize; i++) {
+            pool.get(function (err, db) {
                 assert.ok(!err, err);
-                setImmediate(() => db.detach());
+                
+                setImmediate(function () { 
+                    db.detach();
+                });
             });
         }
         
-        pool.get((err, db) => {
+        pool.get(function (err, db) {
             assert(!err, err);
             
-            db.query('SELECT * FROM RDB$DATABASE', (err, rows) => {
+            db.query('SELECT * FROM RDB$DATABASE', function (err, rows) {
                 assert(!err, err);
+                
                 assert.equal(rows.length, 1);
                 db.detach();
-                setImmediate(() => {
+                setImmediate(function () {
                     assert.equal(pool.dbinuse, 0);
                     done(); 
                 });
@@ -79,37 +87,28 @@ describe('Pooling', () => {
 });
 
 
-describe('Database', () => {
+describe('Database', function () {
     
-    const blobPath = path.join(testDir, 'image.png');
+    var blobPath = path.join(testDir, 'image.png');
+    var db;
     
-    let db;
-    
-    before(done => {
-        Firebird.attach(config, (err, _db) => {
+    before(function (done) {
+        Firebird.attach(config, function (err, _db) {
             if (err) throw err;
             db = _db;
             done();
         });
     });
     
-    after(() => {
+    after(function () {
         if (db) db.detach();
     });
     
-    it('should create table', done => {
-        db.query(`
-            CREATE TABLE test (
-                ID INT, 
-                PARENT BIGINT, 
-                NAME VARCHAR(50), 
-                FILE BLOB, 
-                CREATED TIMESTAMP
-            )
-        `, err => {
+    it('should create table', function (done) {
+        db.query('CREATE TABLE test (ID INT, PARENT BIGINT, NAME VARCHAR(50), FILE BLOB, CREATED TIMESTAMP)', function (err) {
             assert.ok(!err, err);
             
-            db.query('SELECT COUNT(*) FROM test', (err, rows) => {
+            db.query('SELECT COUNT(*) FROM test', function (err, rows) {
                 assert.ok(!err, err);
                 assert.equal(rows[0].count, 0);
                 done();
@@ -117,44 +116,46 @@ describe('Database', () => {
         });
     });
     
-    describe('insert', () => {
-        it('should insert without returning', done => {
-            db.query('INSERT INTO test (ID, NAME, CREATED, PARENT) VALUES(?, ?, ?, ?)', [1, 'Firebird 1', '2014-12-12 13:59', 862304020112911], err => {
+    describe('insert', function () {
+        it('should insert without returning', function (done) {
+            db.query('INSERT INTO test (ID, NAME, CREATED, PARENT) VALUES(?, ?, ?, ?)', [1, 'Firebird 1', '2014-12-12 13:59', 862304020112911], function (err) {
                 assert.ok(!err, err);
                 done();
             });
         });
         
-        it('should insert with returning', done => {
-            db.query('INSERT INTO test (ID, NAME, CREATED, PARENT) VALUES(?, ?, ?, ?) RETURNING ID', [2, 'Firebird 2', currentDate, 862304020112911], (err, row) => {
+        it('should insert with returning', function (done) {
+            db.query('INSERT INTO test (ID, NAME, CREATED, PARENT) VALUES(?, ?, ?, ?) RETURNING ID', [2, 'Firebird 2', currentDate, 862304020112911], function (err, row) {
                 assert.ok(!err, err);
                 assert.equal(row['id'], 2);
                 done();
             });
         });
         
-        it('should insert with blob from stream', done => {
-            db.query('INSERT INTO test (ID, NAME, FILE, CREATED) VALUES(?, ?, ?, ?) RETURNING ID', [3, 'Firebird 3', fs.createReadStream(blobPath), '14.12.2014 12:12:12'], (err, row) => {
+        it('should insert with blob from stream', function (done) {
+            db.query('INSERT INTO test (ID, NAME, FILE, CREATED) VALUES(?, ?, ?, ?) RETURNING ID', [3, 'Firebird 3', fs.createReadStream(blobPath), '14.12.2014 12:12:12'], function (err, row) {
                 assert.ok(!err, err);
                 assert.equal(row['id'], 3);
                 done();
             });
         });
         
-        it('should insert with blob from buffer', done => {
-            db.query('INSERT INTO test (ID, NAME, FILE, CREATED) VALUES(?, ?, ?, ?) RETURNING ID', [4, 'Firebird 4', fs.readFileSync(blobPath), '14.12.2014T12:12:12'], (err, row) => {
+        it('should insert with blob from buffer', function (done) {
+            db.query('INSERT INTO test (ID, NAME, FILE, CREATED) VALUES(?, ?, ?, ?) RETURNING ID', [4, 'Firebird 4', fs.readFileSync(blobPath), '14.12.2014T12:12:12'], function (err, row) {
                 assert.ok(!err, err);
                 assert.equal(row['id'], 4);
                 done();
             });
         });
         
-        describe('verify', () => {
-            it('should select data from inserts', done => {
-                db.query('SELECT * FROM test', (err, rows) => {
+        describe('verify', function () {
+            it('should select data from inserts', function (done) {
+                db.query('SELECT * FROM test', function (err, rows) {
                     assert.ok(!err, err);
                     
-                    const [first, second, third] = rows;
+                    var first = rows[0];
+                    var second = rows[1];
+                    var third = rows[2];
         
                     assert.equal(first.created.getMonth(), 11);
                     assert.equal(first.created.getDate(), 12);
@@ -174,16 +175,16 @@ describe('Database', () => {
                     assert.equal(third.created.getHours(), 12);
                     assert.equal(third.created.getMinutes(), 12);
                     
-                    third.file((err, name, emitter) => {
+                    third.file(function (err, name, emitter) {
                         assert.ok(!err, err);
         
-                        let count = 0;
+                        var count = 0;
         
-                        emitter.on('data', buffer => {
+                        emitter.on('data', function (buffer) {
                             count += buffer.length;
                         });
         
-                        emitter.on('end', () => {
+                        emitter.on('end', function () {
                             assert.equal(count, 5472);
                             done();
                         });
@@ -193,42 +194,42 @@ describe('Database', () => {
         });
     });
        
-    describe('update', () => {
-        it('should update with blob from stream', done => {
-            db.query('UPDATE test SET NAME = ?, FILE = ? WHERE Id = 1', ['Firebird 1 (UPD)', fs.createReadStream(blobPath)], err => {
+    describe('update', function () {
+        it('should update with blob from stream', function (done) {
+            db.query('UPDATE test SET NAME = ?, FILE = ? WHERE Id = 1', ['Firebird 1 (UPD)', fs.createReadStream(blobPath)], function (err) {
                 assert.ok(!err, err);
                 done();
             });
         });
         
-        it('should update with blob from buffer', done => {
-            db.query('UPDATE test SET NAME = ?, FILE = ? WHERE Id = 2', ['Firebird 2 (UPD)', fs.readFileSync(blobPath)], err => {
+        it('should update with blob from buffer', function (done) {
+            db.query('UPDATE test SET NAME = ?, FILE = ? WHERE Id = 2', ['Firebird 2 (UPD)', fs.readFileSync(blobPath)], function (err) {
                 assert.ok(!err, err);
                 done();
             });
         });
         
-        describe('verify', () => {
-            it('should select data from update with blob from stream', done => {
-                db.query('SELECT * FROM test WHERE ID = 1', (err, rows) => {
+        describe('verify', function () {
+            it('should select data from update with blob from stream', function (done) {
+                db.query('SELECT * FROM test WHERE ID = 1', function (err, rows) {
                     assert.ok(!err, err);
                     
-                    const [row] = rows;
+                    var row = rows[0];
                     assert.notEqual(row, undefined);
                     assert.equal(row.id, 1);
                     assert.equal(row.name, 'Firebird 1 (UPD)');
                     assert.equal(typeof(row.file), 'function');
         
-                    row.file((err, name, emitter) => {
+                    row.file(function (err, name, emitter) {
                         assert.ok(!err, err);
         
-                        let count = 0;
+                        var count = 0;
         
-                        emitter.on('data', buffer => {
+                        emitter.on('data', function (buffer) {
                             count += buffer.length;
                         });
         
-                        emitter.on('end', () => {
+                        emitter.on('end', function () {
                             assert.equal(count, 5472);
                             done();
                         });
@@ -236,26 +237,26 @@ describe('Database', () => {
                 });    
             });
             
-            it('should select data from update with blob from buffer', done => {
-                db.query('SELECT * FROM test WHERE ID = 2', (err, rows) => {
+            it('should select data from update with blob from buffer', function (done) {
+                db.query('SELECT * FROM test WHERE ID = 2', function (err, rows) {
                     assert.ok(!err, err);
                     
-                    const [row] = rows;
+                    var row = rows[0];
                     assert.notEqual(row, undefined);
                     assert.equal(row.id, 2);
                     assert.equal(row.name, 'Firebird 2 (UPD)');
                     assert.equal(typeof(row.file), 'function');
         
-                    row.file((err, name, emitter) => {
+                    row.file(function (err, name, emitter) {
                         assert.ok(!err, err);
         
-                        let count = 0;
+                        var count = 0;
         
-                        emitter.on('data', buffer => {
+                        emitter.on('data', function (buffer) {
                             count += buffer.length;
                         });
         
-                        emitter.on('end', () => {
+                        emitter.on('end', function () {
                             assert.equal(count, 5472);
                             done();
                         });
@@ -265,17 +266,11 @@ describe('Database', () => {
         });
     });
     
-    describe('select', () => {
-        it('should select scalar values', done => {
-            db.query(`
-                SELECT 
-                    CAST(123 AS NUMERIC(10,2)) As a, 
-                    MAX(2) AS b, 
-                    COUNT(*) AS c 
-                FROM RDB$DATABASE
-            `, (err, rows) => {
+    describe('select', function () {
+        it('should select scalar values', function (done) {
+            db.query('SELECT CAST(123 AS NUMERIC(10,2)) As a, MAX(2) AS b, COUNT(*) AS c FROM RDB$DATABASE', function (err, rows) {
                 assert.ok(!err, err);
-                const [row] = rows;
+                var row = rows[0];
                 assert.equal(row.a, 123, 'CAST returned an unexpected value.');
                 assert.equal(row.b, 2, 'MAX returned an unexpected value.');
                 assert.notEqual(row.c, 0, 'COUNT returned an unexpected value.');
@@ -283,59 +278,59 @@ describe('Database', () => {
             });
         });
         
-        it('should select rows as arrays', done => {
-            db.execute('SELECT COUNT(*), SUM(ID) FROM test', (err, rows) => {
+        it('should select rows as arrays', function (done) {
+            db.execute('SELECT COUNT(*), SUM(ID) FROM test', function (err, rows) {
                 assert.ok(!err, err);
-                const [row] = rows;
+                var row = rows[0];
                 assert.equal(row[0], 4);
                 assert.equal(row[1], 10);
                 done();
             });    
         });
         
-        it('should select rows as objects', done => {
-            db.query('SELECT COUNT(*), SUM(ID) FROM test', (err, rows) => {
+        it('should select rows as objects', function (done) {
+            db.query('SELECT COUNT(*), SUM(ID) FROM test', function (err, rows) {
                 assert.ok(!err, err);
-                const [row] = rows;
+                var row = rows[0];
                 assert.equal(row.count, 4);
                 assert.equal(row.sum, 10);
                 done();
             });    
         });
         
-        it('should select rows sequentially as arrays', done => {
-            let sum = 0;
-            db.sequentially('SELECT Id FROM test', row => {
+        it('should select rows sequentially as arrays', function (done) {
+            var sum = 0;
+            db.sequentially('SELECT Id FROM test', function (row) {
                 sum += row[0];
-            }, () => {
+            }, function () {
                 assert.equal(sum, 10);
                 done();
             }, true);
         });
         
-        it('should select rows sequentially as objects', done => {
-            let sum = 0;
-            db.sequentially('SELECT Id FROM test', row => {
+        it('should select rows sequentially as objects', function (done) {
+            var sum = 0;
+            db.sequentially('SELECT Id FROM test', function (row) {
                 sum += row.id;
-            }, () => {
+            }, function () {
                 assert.equal(sum, 10);
                 done();
             });
         });
     });
     
-    describe('transaction', () => {
+    describe('transaction', function () {
        
-        it('should rollback', done => {
-            db.transaction((err, transaction) => {
+        it('should rollback', function (done) {
+            db.transaction(function (err, transaction) {
                 assert(!err, err);
-                transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [5, 'Transaction 1'], err => {
+                transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [5, 'Transaction 1'], function (err) {
                     assert.ok(!err, err);
-                    transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [6, 'Transaction 2'], err => {
+                    transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [6, 'Transaction 2'], function (err) {
                         assert.ok(!err, err);
-                        transaction.query('INSERT INTO test_fail (ID, NAME) VALUES(?, ?)', [7, 'Transaction 3'], err => {
+                        transaction.query('INSERT INTO test_fail (ID, NAME) VALUES(?, ?)', [7, 'Transaction 3'], function (err) {
                             assert.ok(err);
-                            transaction.rollback(err => {
+                            transaction.rollback(function (err) {
                                 assert.ok(!err, err);
                                 verify(done, 4);
                             });
@@ -345,16 +340,16 @@ describe('Database', () => {
             });
         });
 
-        it('should commit', done => {
-            db.transaction((err, transaction) => {
+        it('should commit', function (done) {
+            db.transaction(function (err, transaction) {
                 assert(!err, err);
-                transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [5, 'Transaction 1'], err => {
+                transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [5, 'Transaction 1'], function (err) {
                     assert.ok(!err, err);
-                    transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [6, 'Transaction 2'], err => {
+                    transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [6, 'Transaction 2'], function (err) {
                         assert.ok(!err, err);
-                        transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [7, 'Transaction 3'], err => {
+                        transaction.query('INSERT INTO test (ID, NAME) VALUES(?, ?)', [7, 'Transaction 3'], function (err) {
                             assert.ok(!err, err);
-                            transaction.commit(err => {
+                            transaction.commit(function (err) {
                                 assert.ok(!err, err);
                                 verify(done, 7);
                             });
@@ -365,9 +360,9 @@ describe('Database', () => {
         });
         
         function verify(callback, count) {
-            db.query('SELECT COUNT(*) FROM test', (err, rows) => {
+            db.query('SELECT COUNT(*) FROM test', function (err, rows) {
                 assert.ok(!err, err);
-                const [row] = rows;
+                var row = rows[0];
                 assert.equal(row.count, count);
                 callback();
             });
