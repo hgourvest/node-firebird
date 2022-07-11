c16cb347d37ecf8756cd5661f18d29d5e62576d8
Kevin Brown <kevin.brown@exogee.com>
2022-07-11 18:58:35 +1000

    Added configurable character set for connections.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -51,6 +51,37 @@ declare module 'node-firebird' {
         rollbackRetaining(callback?: SimpleCallback): void;
     }
 
+    export type SupportedCharacterSet = |
+        'NONE' |
+        'CP943C' |
+        'DOS737' |
+        'DOS775' |
+        'DOS858' |
+        'DOS862' |
+        'DOS864' |
+        'DOS866' |
+        'DOS869' |
+        'GB18030' |
+        'GBK' |
+        'ISO8859_2' |
+        'ISO8859_3' |
+        'ISO8859_4' |
+        'ISO8859_5' |
+        'ISO8859_6' |
+        'ISO8859_7' |
+        'ISO8859_8' |
+        'ISO8859_9' |
+        'ISO8859_13' |
+        'KOI8R' |
+        'KOI8U' |
+        'TIS620' |
+        'UTF8' |
+        'WIN1255' |
+        'WIN1256' |
+        'WIN1257' |
+        'WIN1258' |
+        'WIN_1258';
+
     export interface Options {
         host?: string;
         port?: number;
@@ -61,6 +92,7 @@ declare module 'node-firebird' {
         role?: string;
         pageSize?: number;
         retryConnectionInterval?: number;
+        encoding?: SupportedCharacterSet;
     }
 
     export interface SvcMgrOptions extends Options {
